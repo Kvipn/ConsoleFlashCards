bc882b6deb1108d8246c3292c02c2a83ca8a8026
Kvipn <56080886+Kvipn@users.noreply.github.com>
2024-09-18 10:10:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleFlashCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
